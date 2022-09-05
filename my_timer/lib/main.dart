2eed11e035858a856c938fd6a7c3966f6355e969
FOMEXISH <fomexish99@gmail.com>
2022-09-06 02:50:58 +0900

    complete commit

--- my_timer/lib/main.dart
+++ my_timer/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,40 +10,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Timer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'MY TIMER'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +32,169 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Timer? _timer;
+  final _limitController = TextEditingController(text: '300');
+  final _delayController = TextEditingController(text: '30');
+  final _player = AudioPlayer();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  late int _limit;
+  late int _delay;
+  var _time = 0;
+  var _isPlaying = false;
+  var _isWait = false;
+
+  var _btnIcon = Icons.play_arrow;
+  var _btnColor = Colors.blue;
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void alarm() async {
+    _isWait = !_isWait;
+    _time = 0;
+    await _player.play(AssetSource(_isWait ? 'end.mp3' : 'start.mp3'));
+  }
+
+  void _start() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (!_isWait && _limit - 1 <= _time) {
+          alarm();
+        } else if (_isWait && _delay - 1 <= _time) {
+          alarm();
+        } else {
+          _time++;
+        }
+      });
     });
+  }
+
+  void _reset() {
+    setState(() {
+      _isPlaying = false;
+      _isWait = false;
+      _timer?.cancel();
+      _time = 0;
+    });
+  }
+
+  void _click() {
+    _isPlaying = !_isPlaying;
+    _limitController.text =
+        _limitController.text == '' ? '300' : _limitController.text;
+    _delayController.text =
+        _delayController.text == '' ? '30' : _delayController.text;
+    _limit = int.parse(_limitController.text);
+    _delay = int.parse(_delayController.text);
+    if (_isPlaying) {
+      _btnIcon = Icons.stop;
+      _btnColor = Colors.red;
+      _start();
+    } else {
+      _btnIcon = Icons.play_arrow;
+      _btnColor = Colors.blue;
+      _reset();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverFillRemaining(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          '${_time ~/ 60 < 10 ? '0${_time ~/ 60}' : '${_time ~/ 60}'} ${_time % 60 < 10 ? '0${_time % 60}' : '${_time % 60}'}',
+                          style: TextStyle(
+                            fontSize: 100.0,
+                            fontWeight: FontWeight.bold,
+                            color: !_isWait ? Colors.black : Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(8.0, 0.0, 4.0, 4.0),
+                          child: TextField(
+                            controller: _limitController,
+                            decoration: const InputDecoration(
+                              labelText: 'LIMIT',
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(),
+                              ),
+                            ),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp("[0-9]")),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(4.0, 0.0, 8.0, 4.0),
+                          child: TextField(
+                            controller: _delayController,
+                            decoration: const InputDecoration(
+                              labelText: 'DELAY',
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(),
+                              ),
+                            ),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp("[0-9]")),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 8.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        FocusScope.of(context).unfocus();
+                        _click();
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: _btnColor,
+                          minimumSize: const Size(double.infinity, 50.0)),
+                      child: Icon(_btnIcon),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
